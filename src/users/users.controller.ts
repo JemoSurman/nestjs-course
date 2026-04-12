642b6637c47fef52bcc682007814cdf475a4c29b
JemoSurman <jemosurman@gmail.com>
2026-04-12 15:38:37 +0400

    built in Pipes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Param, Query } from "@nestjs/common";
+import { Controller, Get, Post, Param, Query, ParseIntPipe, ParseArrayPipe, DefaultValuePipe } from "@nestjs/common";
 import { UsersService } from "./users.service";
 
 @Controller('users')
@@ -7,26 +7,25 @@ export class UsersController {
     constructor(private readonly usersService: UsersService){}
 
     @Get()
-    getUsers(@Query() query: any){
-        if(query.gender && query.age){
-            return this.usersService.users.filter(user => user.gender === query.gender && user.age === +query.age);
-        }
-        if(query.gender){
-            return this.usersService.users.filter(user => user.gender === query.gender);
-        }
+    getUsers(
+        @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number, 
+        @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number
+    )
+    {
+        
         return this.usersService.getAllUsers();
     }
 
     @Get(':id')
-    getUsersById(@Param('id') id: any){
-        return this.usersService.getUsersById(+id);
+    getUsersById(@Param('id', ParseIntPipe) id: number){
+        console.log(typeof id, id);
+        return this.usersService.getUsersById(id);
     }
 
     @Post()
     createUsers(){
         const user = {id: 4, name: 'anna', age: 24, gender: 'female', isMarried: false};
         return this.usersService.createUser(user);    
-        return 'A new user has been created!'; 
     }
      
 }
