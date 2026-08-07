f34ec3634425ff883f842ae364034d869bc9fb83
JemoSurman <jemosurman@gmail.com>
2026-08-07 18:46:55 +0400

    Using Authorize Guard

--- src/auth/guards/authorize.guard.ts
+++ src/auth/guards/authorize.guard.ts
@@ -1,18 +1,41 @@
-import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException, Inject } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import type { ConfigType } from "@nestjs/config";
 import { Request } from "express";
-import { Observable } from "rxjs";
+import authConfig from "../config/auth.config";
 
 @Injectable()
 export class AuthorizedGuard implements CanActivate {
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    constructor(
+        private readonly jwtService: JwtService,
+
+        @Inject(authConfig.KEY)
+        private readonly authConfiguration: ConfigType<typeof authConfig>       
+    ){}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
         //1. EXTRACT REQUEST FROM EXECUTION CONTEXT
         const request:Request = context.switchToHttp().getRequest();
 
         //2. EXTRACT TOKEN FROM THE REQUEST HEADER
         //Bearer actual-json-web-token = ['Bearer', 'actual-json-web-token']
         const token = request.headers.authorization?.split(' ')[1];
-        console.log(token);
+        
         //3. VALIDATE TOKEN AND PROVIDE / DENY ACCESS
+        if(!token){
+            throw new UnauthorizedException();
+        }
+        
+        try{
+            const payload = await this.jwtService.verifyAsync(token, {
+                secret: this.authConfiguration.secret
+            }) 
+            request['user'] = payload;
+
+        }catch(error){
+            throw new UnauthorizedException();
+        }
+
         return true;
     }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,19 @@
-import { Controller, Get, Post,Body, Delete, Param, ParseIntPipe, Query, UseGuards} from "@nestjs/common";
+import { Controller, Get, Delete, Param, ParseIntPipe, Query, UseGuards} from "@nestjs/common";
 import { UsersService } from "./users.service";
-import { CreateUserDto } from "./dtos/create-user.dto";
+
 import { PaginationQueryDto } from "../common/pagination/dto/pagination-query.dto";
 import { AuthorizedGuard } from "../auth/guards/authorize.guard";
 @Controller('users')
+@UseGuards(AuthorizedGuard)
 export class UsersController {
 
     constructor(private readonly usersService: UsersService){}
 
-    @UseGuards(AuthorizedGuard)
     @Get()
     getUsers(@Query() pageQueryDto: PaginationQueryDto){
         return this.usersService.getAllUsers(pageQueryDto);
     }
 
-    @UseGuards(AuthorizedGuard)
     @Get(':id')
     getUserById(@Param('id', ParseIntPipe) id: number){
         return this.usersService.findUserById(id);
@@ -31,6 +30,5 @@ export class UsersController {
         this.usersService.deleteUser(id);
     }
 
-    
-     
+           
 }
